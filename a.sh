6252140fc523cef6cc97673e97e389cfd14615ad
Animae-Magnae-Prodigus <EnigmaticJaga@proton.com>
2024-12-08 20:52:26 +0800

    update

--- a.sh
+++ a.sh
@@ -1,7 +1,7 @@
-git add *md
-git add _posts/science-fiction/*md
-git add *py
-git add *yml
+# git add *md
+# git add _posts/science-fiction/*md
+# git add *py
+# git add *yml
 git add a.sh
 git commit -m "update"
 git push
