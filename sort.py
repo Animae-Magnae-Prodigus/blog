2ef9c65c0090c27c500b2cf3cc3bf46074c6598a
Animae-Magnae-Prodigus <EnigmaticJaga@proton.com>
2025-07-15 09:06:50 +0800

    update

--- sort.py
+++ sort.py
@@ -3,6 +3,7 @@ import sys
 import re
 sep = ", "
 result = []
+AuthorTitles = []
 with open(fileName:="HN.tsv", "r") as f:
 
     reader = csv.DictReader(f, dialect=csv.excel_tab, )
@@ -17,9 +18,15 @@ with open(fileName:="HN.tsv", "r") as f:
             if number := re.findall(r"^(\d{4})$", i.strip()):
                 year = int(number[0])
                 break
-        result.append((year, r))
+        r["Author"] = r["Author"] .strip()
+        r["Title"] = r["Title"] .strip()
+        r["Directory"] = r["Directory"] .strip()
+        AuthorTitle = r["Author"]+r["Title"] + r["Tags"]
+        if AuthorTitle not in AuthorTitles:
+            result.append((year, r))
+            AuthorTitles.append(AuthorTitle)
 
-result.sort(key=lambda x: x[0])
+result.sort(key=lambda x: x[0], reverse=True)
 # result = [x[1] for x in result]
 with open( fileName+sys.argv[0]+".tsv", "w") as f:
     w = csv.DictWriter(f, dialect=csv.excel_tab, fieldnames=fieldnames)
